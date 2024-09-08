97bf25e4cd32c8e4da3e7590b22092cd3bd575ef
Kenji Saito <ken-yo@mbr.nifty.com>
2024-09-08 09:49:29 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@ if [ $result -ne 0 ]; then
 fi
 echo ""
 pwd
-corepack use pnpm@latest && pnpm install && pnpm up -r && pnpm lint-fix && pnpm build && pnpm build-storybook
+corepack use pnpm@latest && pnpm install && pnpm up -r && pnpm lint-fix && pnpm build
 result=$?
 if [ $result -ne 0 ]; then
   cd "${CUR}" || exit
