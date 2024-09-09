4197d753908c02df1ab9de8ead3574448dbe5f4d
Kenji Saito <ken-yo@mbr.nifty.com>
2024-09-09 21:56:59 +0900

    Bumps node modules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -63,15 +63,7 @@ export default tseslint.config(
       '@stylistic/jsx/jsx-indent': ['error', 2],
       "comma-dangle": ["error", "always-multiline"],
       "quotes": ["error", "single"],
-      'semi': ["error", "always"],
-      'import/namespace': 'off',
-      'import/named': 'off',
-      'import/no-named-as-default': 'off',
-      'import/no-named-as-default-member': 'off',
-      'import/no-unresolved': 'off',
-      'import/no-duplicates': 'off',
-      'import/default': 'off',
-      'import/export': 'off',
+      semi: ["error", "always"],
     }
   },
 );
